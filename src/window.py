e0648fddf114ff0cf49782155d70df1d0d942801
kheldae <taho.gougeon@gmail.com>
2026-01-15 17:28:10 +0100

    splitted the interfaces for now

--- src/window.py
+++ src/window.py
@@ -38,7 +38,6 @@ class StagWindow(Adw.ApplicationWindow):
     # toolbar kids
     # TODO write the whole interface first
     editor_stack = Gtk.Template.Child()
-    entity_editor = Gtk.Template.Child()
 
     def __init__(self, **kwargs) -> None:
         self._project: StagProject = None
